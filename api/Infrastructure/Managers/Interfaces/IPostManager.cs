980575111f39c9fb68780f6825e2bb915e86cb3e
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-01-13 15:42:39 -0500

    add edit post endPoint

--- api/Infrastructure/Managers/Interfaces/IPostManager.cs
+++ api/Infrastructure/Managers/Interfaces/IPostManager.cs
@@ -5,5 +5,6 @@ namespace api.Infrastructure.Managers.Interfaces
     public interface IPostManager
     {
         Task<int> CreatePostAsync(CreatePostDto post, string userId);
+        Task UpdatePostAsync(CreatePostDto postUpdate, int postId, string userId);
     }
 }
